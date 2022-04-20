3af670fbf14e433e7cccc7379ba46d1401a2cb0e
anna <a.sahradyan72@gmail.com>
2022-04-20 15:07:16 +0400

    finish project

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,10 +14,20 @@ const About = () => {
                             <Card.Img variant="top" src='/images/userWoman.png'style={{ width: '2.5rem',height:"2.5rem" }}/>
                             <Card.Title style={{ marginLeft:'200px' }}>ABOUT ME</Card.Title>
                             <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.  Some quick example text to build on the card title and make up the bulk of
-                                the card's   Some quick example text to build on the card title and make up the bulk of
-                                the card's content.<content className=""></content>
+                               <i><b>Hello!
+                                   My name is Anahit Paruryan.
+                                   As a nurse, I changed careers and enrolled in online programming courses, I enjoy every aspect of it and I am eager to get started.<br/>
+                                   You can find detailed information about me here:
+                                   <hr/>
+                                   GitHub: https://github.com/anna-sahradyan
+                                   <hr/>
+                                   Resume:https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:e1a19f2a-6aaf-4da0-83ab-3af3f3f94cc2
+                                   <hr/>
+                                   Thanks in advance!
+                                   Sincerely,
+                                   Anna (This is how you may address me)
+
+                               </b></i> <content className=""></content>
                             </Card.Text>
                             <Link to={'/'}><Button variant="outline-primary" className={s.btn}
                             >Go To  The Gallery</Button></Link>
